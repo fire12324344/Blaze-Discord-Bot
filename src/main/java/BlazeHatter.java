48b60345192c1ae784607507d4cf6dd4c79c3650
fire12324344 <theodore.yang@mesotech.ca>
2023-01-11 13:16:00 -0800

    e

--- src/main/java/BlazeHatter.java
+++ src/main/java/BlazeHatter.java
@@ -1,27 +1,38 @@
-package com.company;
+import net.dv8tion.jda.api.entities.Message;
+
 import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
-import java.awt.image.*;
 import java.io.File;
 import java.io.IOException;
-
 
 public class BlazeHatter extends JFrame {
     private ImageIcon icon;
     private JLabel label;
-    private ImageIcon icon2;
-    private JLabel label2;
-
-    public BlazeHatter() throws IOException {
+    private ImageIcon input;
+    private JLabel inputLabel;
+    final int ICON_WIDTH = 143;
+    final int ICON_HEIGHT = 148;
+    public BlazeHatter() {
         setLayout(null);
-        icon = new ImageIcon(getClass().getResource("blaze_hat.png"));
+        icon = new ImageIcon(getClass().getResource("blazehat.png"));
         label = new JLabel(icon);
-        label.setBounds(0,0,143,148);
+        label.setBounds(0, 0, ICON_WIDTH, ICON_HEIGHT);
         add(label);
-        icon2 = new ImageIcon(ImageIO.read(new File("src/com/company/test.png")) .getScaledInstance(72, 74, Image.SCALE_FAST));
-        label2 = new JLabel(icon2);
-        label2.setBounds(0, 0, 143,148);
-        add(label2);
+    }
+    public int[] run() throws IOException {
+        input = new ImageIcon(ImageIO.read(new File("src/main/resources/input.png")));
+        int newHeight =input.getIconHeight()*ICON_WIDTH/input.getIconWidth();
+        input = new ImageIcon(ImageIO.read(new File("src/main/resources/input.png")).getScaledInstance(ICON_WIDTH/2,newHeight/2, Image.SCALE_FAST ));
+        inputLabel = new JLabel(input);
+        inputLabel.setBounds(0,ICON_HEIGHT/4,ICON_WIDTH,newHeight/2);
+        add(inputLabel);
+        return new int[]{
+                ICON_WIDTH,
+                Math.max(newHeight/2 + (ICON_HEIGHT/4), ICON_HEIGHT)
+        };
+    }
+    public void undo() {
+        remove(inputLabel);
     }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,35 +1,77 @@
-import net.dv8tion.jda.api.AccountType;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.api.entities.emoji.Emoji;
+import net.dv8tion.jda.api.entities.emoji.UnicodeEmoji;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.dv8tion.jda.api.utils.FileUpload;
 
-
+import javax.imageio.ImageIO;
 import javax.security.auth.login.LoginException;
+import java.awt.*;
+import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.IOException;
+import java.util.regex.Pattern;
 
 public class Main extends ListenerAdapter {
     final String prefix = "!";
     public static void main(String[] args) throws LoginException {
+        Message.suppressContentIntentWarning();
         final String api = "MTAxNjg4NDM1ODA1NDkzNjY1Ng.G3kAxJ.S4S-_8-I0_xoBPBsXHlMZHoRG6LRRmjLpEhzGA";
         JDABuilder builder = JDABuilder.createDefault(api);
         builder.addEventListeners(new Main());
         builder.build();
-
-
     }
 
     @Override
     public void onMessageReceived(MessageReceivedEvent e) {
-      switch (e.getMessage().getContentRaw()) {
-          case "1352": e.getChannel().sendMessage("i hate people of coler").queue(); break;
+        if (Pattern.matches("[u,o] *w *[u,o]", e.getMessage().getContentRaw())) {
+            System.out.println("match");
+            e.getChannel().sendMessage("NO FURRIES").queue();
+        }
+        switch (e.getMessage().getContentRaw()) {
+          case "1352":
+              e.getChannel().sendMessage("i hate people of coler").queue();
+              break; //api
           case prefix + "help":
               e.getChannel().sendMessage("No, fuck you").queue();
               break;
           case prefix + "blaze":
               e.getChannel().sendMessage("blaze").addFiles(FileUpload.fromData(new File("stuff/blaze.png"), "image.png")).queue();
+              break;
+          case prefix + "blazesex":
+              e.getMessage().addReaction(Emoji.fromUnicode("U+1F928")).queue();
+              e.getChannel().sendMessage("i'm 16").queue();
+          case prefix + "blazehat":
+              BlazeHatter blazehat = new BlazeHatter();
+              try {
+                  e.getMessage().getAttachments().get(0).downloadToFile(new File("src/main/resources/input.png"));
+              } catch (IndexOutOfBoundsException noImage) {
+                  break;
+              }
+              int[] tem = new int[2];
+              try {
+                  Thread.sleep(400);
+                  tem = blazehat.run();
+                  Thread.sleep(400);
+              } catch (InterruptedException | IOException interruptedException) {
+                  interruptedException.printStackTrace();
+                  blazehat.undo();
+              }
 
+              Container pane = blazehat.getContentPane();
+              pane.setSize(tem[0], tem[1]);
+              BufferedImage im = new BufferedImage(pane.getWidth(), pane.getHeight(), BufferedImage.TYPE_INT_ARGB);
+              pane.paint(im.getGraphics());
+              try {
+                  ImageIO.write(im, "PNG", new File("src/main/resources/output.png"));
+              } catch (IOException ioException) {
+                  ioException.printStackTrace();
+              }
+              e.getChannel().sendMessage("").addFiles(FileUpload.fromData(new File("src/main/resources/output.png"), "blazed.png")).queue();
+              blazehat.undo();
+              break;
       }
 
         }
